91368bce6be2ef74a06d2709c86f75c658e553b0
Yu Zhengwen <yuzhengwen0217@gmail.com>
2023-09-12 15:43:06 +0800

    gallery added

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,22 @@
+
+
+//floating header when scroll
+/*window.addEventListener('scroll', makeFloating);
+function makeFloating(){
+
+    var header = document.querySelector("header");
+    header.classList.toggle("floating", window.scrollY>0);
+}
+
+document.querySelectorAll(".header_button").forEach((button) => {
+    console.log(button.className + button.href+ " \n" + window.location.href);
+    if (button.href === window.location.href) {
+        button.classList.add("active");
+        button.setAttribute("aria-current", "page");
+    }
+});*/
+
+//boxes sliding in & scroll up button showing when scroll
 const boxes = document.querySelectorAll(".box");
 
 window.addEventListener('scroll', checkBoxes);
@@ -24,10 +43,21 @@ function displayScrollTop(){
     console.log(scrollTop.className);
 }
 
+//on click of scroll up button
 //good practice to add listener instead of using html onclick=""
 document.getElementById ("scroll_top").addEventListener ("click", scrollTop, false);
 
 function scrollTop() {
     console.log("UP");
     window.scrollTo({ top: 0, behavior: 'smooth' });
+}
+
+//gallery
+document.querySelectorAll(".gallery img").addEventListener ("mouseover", darken(), false);
+function darken(){
+    const selected = document.querySelector(".gallery img:hover");
+    console.log(selected.className);
+    const body = document.querySelector("body");
+    body.classList.add("darken");
+    selected.classList.add("selected");
 }
