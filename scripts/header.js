9643e7e63c58b34e5c2b303cb41ad1c0b387e938
Yu Zhengwen <yuzhengwen0217@gmail.com>
2023-09-12 14:48:29 +0800

    Updates

--- scripts/header.js
+++ scripts/header.js
@@ -1,6 +1,7 @@
 window.addEventListener('scroll', makeFloating);
 
 function makeFloating(){
+
     var header = document.querySelector("header");
     header.classList.toggle("floating", window.scrollY>0);
 }
--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,7 @@
 const boxes = document.querySelectorAll(".box");
 
-window.addEventListener('scroll', checkBoxes)
+window.addEventListener('scroll', checkBoxes);
+window.addEventListener('scroll', displayScrollTop);
 
 function checkBoxes() {
     const triggerBottom = window.innerHeight / 5 * 4;
@@ -14,4 +15,19 @@ function checkBoxes() {
             box.classList.remove("show");
         }
     })
+}
+
+function displayScrollTop(){
+    
+    var scrollTop = document.querySelector("#scroll_top");
+    scrollTop.classList.toggle("show", window.scrollY>0);
+    console.log(scrollTop.className);
+}
+
+//good practice to add listener instead of using html onclick=""
+document.getElementById ("scroll_top").addEventListener ("click", scrollTop, false);
+
+function scrollTop() {
+    console.log("UP");
+    window.scrollTo({ top: 0, behavior: 'smooth' });
 }
