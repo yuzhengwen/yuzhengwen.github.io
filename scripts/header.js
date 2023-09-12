6ef91f48f960c9c248a2ced48231dae3497758f9
Yu Zhengwen <yuzhengwen0217@gmail.com>
2023-09-12 09:44:49 +0800

    Added active header highlight

--- scripts/header.js
+++ scripts/header.js
@@ -4,3 +4,10 @@ function makeFloating(){
     var header = document.querySelector("header");
     header.classList.toggle("floating", window.scrollY>0);
 }
+
+document.querySelectorAll(".header_button").forEach((button) => {
+    if (button.href === window.location.href) {
+        button.classList.add("active");
+        button.setAttribute("aria-current", "page");
+    }
+});
