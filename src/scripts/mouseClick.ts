d59c33b5e3638be385d660fa0ccf8f86924b5813
Yu Zhengwen <yuzhengwen0217@gmail.com>
2026-09-07 15:46:41 +0800

    Changed click effect abs to fixed. this was causing flickering on mobile when clicking on right side of screen, causing document to expand right for a split second

--- src/scripts/mouseClick.ts
+++ src/scripts/mouseClick.ts
@@ -1,8 +1,8 @@
 document.onclick = function (e) {
   const span = document.createElement("span");
   span.className = "click-effect";
-  span.style.left = e.pageX + "px";
-  span.style.top = e.pageY + "px";
+  span.style.left = e.clientX + "px";
+  span.style.top = e.clientY + "px";
   document.body.appendChild(span);
   setTimeout(() => {
     span.remove();
